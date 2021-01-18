4e0ad4b32c1f4048a6b07af49b8aa6645b688908
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2021-01-18 20:28:40 +0200

    creating fist app in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Odometer extends React.Component{
+
+  constructor() {
+    super();
+    this.state = {counter:0};
+   
+  }
+
+  odometerCounter(value) {
+    if(this.state.counter+value > 9999){
+      this.state.counter=this.state.counter+value-10001
+}
+
+    this.setState({
+      counter: this.state.counter + value
+    });
+  }
+  render(){
+    return(
+      <>
+      <button className='btn' onClick={()=>{
+        this.odometerCounter(1)
+      }}>click to  add 1 </button>
+      <button  className='btn'  onClick={()=>{
+        this.odometerCounter(10)
+      }}>click to  add 10 </button>
+     <button className='btn'  onClick={()=>{
+        this.odometerCounter(100)
+      }}>click to  add 100 </button>
+      <button  className='btn' onClick={()=>{
+        this.odometerCounter(1000)
+      }}>click to  add 1000 </button>
+      <p className='para'>
+        {String(this.state.counter).padStart(4,0)}
+      </p>
+      </>
+    )
+  }   
+}
+
+function Header(props) {
+
+  return (
+    <h1 className='header-one'> {props.greeting}</h1>
+  );
+};
+function Footer(props) {
+  return (
+    <h3 className='footer-one' > Trade Mark all by {props.trademark} </h3>
+  );
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header greeting='Hello from React ' />
+      <Odometer/>
+      <Footer trademark='written by hadeel  ' />
+    </>
   );
 }
 
